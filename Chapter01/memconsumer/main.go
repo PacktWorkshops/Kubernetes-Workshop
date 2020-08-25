b83e7dbf304f1f1bd2fd310a551c02a26c6b712c
prachijain1992 <57354930+prachijain1992@users.noreply.github.com>
2020-08-25 14:09:06 +0530

    Update main.go

--- Chapter01/memconsumer/main.go
+++ Chapter01/memconsumer/main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-	"time"
+        "fmt"
+        "strings"
+        "time"
 )
 
 func main() {
-	var longStrs []string
-	times := 50
-	for i := 1; i <= times; i++ {
-		fmt.Printf("===============%d===============\n", i)
-		// each time we build a long string to consume 1MB (1000000 * 1byte) RAM
-		longStrs = append(longStrs, buildString(1000000, byte(i)))
-	}
-	// hold the application to exit in 1 hour
-	time.Sleep(3600 * time.Second)
+        var longStrs []string
+        times := 50
+        for i := 1; i <= times; i++ {
+                fmt.Printf("===============%d===============\n", i)
+                // each time we build a long string to consume 1MB (1000000 * 1byte) RAM
+                longStrs = append(longStrs, buildString(1000000, byte(i)))
+        }
+        // hold the application to exit in 1 hour
+        time.Sleep(3600 * time.Second)
 }
 
 // buildString build a long string with a length of `n`.
 func buildString(n int, b byte) string {
-	var builder strings.Builder
-	builder.Grow(n)
-	for i := 0; i < n; i++ {
-		builder.WriteByte(b)
-	}
-	return builder.String()
+        var builder strings.Builder
+        builder.Grow(n)
+        for i := 0; i < n; i++ {
+                builder.WriteByte(b)
+        }
+        return builder.String()
 }

c17811bbb90258cdd3aa78a42f0f712d591a78a9
prachijain1992 <57354930+prachijain1992@users.noreply.github.com>
2020-08-25 14:19:45 +0530

    Update main.go

--- Chapter01/Activity1.01/pageview-modified/main.go
+++ Chapter01/Activity1.01/pageview-modified/main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+        "fmt"
+        "log"
+        "net/http"
 
-	"github.com/go-redis/redis/v7"
+        "github.com/go-redis/redis/v7"
 )
 
 var dbClient *redis.Client
 var key = "pv"
 
 func init() {
-	dbClient = redis.NewClient(&redis.Options{
-		Addr:     "db:6379",
-	})
+        dbClient = redis.NewClient(&redis.Options{
+                Addr:     "db:6379",
+        })
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+        http.HandleFunc("/", handler)
+        log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Ping from %s", r.RemoteAddr)
-	pageView, err := dbClient.Incr(key).Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Fprintf(w, "Hello, you're vistor #%v.\n", pageView)
+        log.Printf("Ping from %s", r.RemoteAddr)
+        pageView, err := dbClient.Incr(key).Result()
+        if err != nil {
+                panic(err)
+        }
+        fmt.Fprintf(w, "Hello, you're visitor #%v.\n", pageView)
 }

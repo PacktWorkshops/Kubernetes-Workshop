10ab6ca1444e3e645c760fa6658e54d570438d5c
prachijain1992 <57354930+prachijain1992@users.noreply.github.com>
2020-08-25 14:15:36 +0530

    Update main.go

--- Chapter01/Activity1.01/pageview/main.go
+++ Chapter01/Activity1.01/pageview/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+        "fmt"
+        "log"
+        "net/http"
 )
 
 var pageView int64
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+        http.HandleFunc("/", handler)
+        log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Ping from %s", r.RemoteAddr)
-	pageView++
-	fmt.Fprintf(w, "Hello, you're visitor #%d !", pageView)
+        log.Printf("Ping from %s", r.RemoteAddr)
+        pageView++
+        fmt.Fprintf(w, "Hello, you're visitor #%d !", pageView)
 }
